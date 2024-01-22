b4e2bcf2e5d764dab07f53efa424bee8bdf1157b
Gregers Rørdam <gregers@rordam.dk>
2024-01-22 21:31:32 +0100

    Add .local/bin to PATH

--- zsh/custom/env.zsh
+++ zsh/custom/env.zsh
@@ -1,4 +1,5 @@
 export CDPATH=..
 export VISUAL=nvim
 export EDITOR="${VISUAL}"
-[ -f "/home/gsr/.ghcup/env" ] && source "/home/gsr/.ghcup/env" # ghcup-env
+[ -f "$HOME/.ghcup/env" ] && source "$HOME/.ghcup/env" # ghcup-env
+export PATH="$HOME/.local/bin:$PATH"
