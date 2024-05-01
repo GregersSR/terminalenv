cdd8657882ff6b5853f492ed9bb423a3075c35b3
Gregers Rørdam <gregers@rordam.dk>
2024-05-01 23:36:35 +0200

    Port config to home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,26 +1,15 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "gsr";
-  home.homeDirectory = "/home/gsr";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
+  home = {
+    username = "gsr";
+    homeDirectory = "/home/gsr";
+    stateVersion = "23.11";
+  };
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
@@ -36,19 +25,14 @@
     # '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+      ".config/nvim".source = ../config/nvim;
+  };
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+  home.shellAliases = {
+    ll = "ls -lAh";
+    info = "info --vi-keys";
+    grep = "grep --color=auto --exclude-dir={.git,.idea}";
   };
 
   # Home Manager can also manage your environment variables through
@@ -68,9 +52,49 @@
   #  /etc/profiles/per-user/gsr/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "nvim";
+    VISUAL = "nvim";
+    CDPATH = "..";
   };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  programs.neovim = {
+    enable = true;
+    vimAlias = true;
+  };
+
+  programs.bat.enable = true;
+  programs.eza.enable = true;
+
+  programs.zsh = {
+    enable = true;    
+    oh-my-zsh = {
+      enable = true;
+      plugins = ["git"];
+      theme = "sunrise";
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Gregers Rørdam";
+    userEmail = "gregers@rordam.dk";
+    extraConfig = {
+      core = {
+        autocrlf = "input";
+	editor = "nvim";
+      };
+      pull.ff = "only";
+      init.defaultBranch = "main";
+    };
+    difftastic.enable = true;
+  };
+
+  programs.ssh = {
+    enable = true;
+    hashKnownHosts = false;
+    includes = [ ".ssh/config.d/*" ];
+  };
 }
