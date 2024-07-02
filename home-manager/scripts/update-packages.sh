da8632cf82fba0049a2456728ecbe4a7d42265ed
Gregers Rørdam <gregers@rordam.dk>
2024-07-03 00:00:59 +0200

    Add printing + fwupdmgr to update-packages.sh

--- home-manager/scripts/update-packages.sh
+++ home-manager/scripts/update-packages.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-command -v apt-get && sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove -y
-command -v snap && sudo snap refresh
+function checkAndPrint {
+	local cmd="$1"
+	command -v "$cmd" >/dev/null && printf "=== Upgrading: ${cmd} ===\n"
+}
+
+checkAndPrint apt-get && sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove -y
+checkAndPrint snap && sudo snap refresh
+checkAndPrint fwupdmgr && sudo fwupdmgr update
