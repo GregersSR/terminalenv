2361e44ba479724b3ce2eb3468d5fb80ebc0b925
Gregers Rørdam <gregers@rordam.dk>
2024-07-04 22:26:58 +0200

    Upgrade Pacstall packages

--- home-manager/scripts/update-packages.sh
+++ home-manager/scripts/update-packages.sh
@@ -2,9 +2,10 @@
 
 function checkAndPrint {
 	local cmd="$1"
-	command -v "$cmd" >/dev/null && printf "=== Upgrading: ${cmd} ===\n"
+	command -v "$cmd" >/dev/null && printf "\n\n====== Upgrading: ${cmd} ======\n\n"
 }
 
 checkAndPrint apt-get && sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove -y
 checkAndPrint snap && sudo snap refresh
 checkAndPrint fwupdmgr && sudo fwupdmgr update
+checkAndPrint pacstall && sudo pacstall -U && sudo pacstall -Up
